a4113ec59df3367f826ee2246091d8b752118caa
aren talb <aren.talb00@gmail.com>
2024-03-15 10:10:54 +0300

    Change skills data

--- src/components/layout/home/SkillsSection.tsx
+++ src/components/layout/home/SkillsSection.tsx
@@ -7,41 +7,21 @@ interface SkillProps {
 }
 
 const skillsData: SkillProps[] = [
-  {
-    id: 1,
-    name: "Angular",
-    imgURL: "/skills/angular.svg",
-  },
-  {
-    id: 2,
-    name: "CSS",
-    imgURL: "/skills/Css.svg",
-  },
-  {
-    id: 3,
-    name: "HTML",
-    imgURL: "/skills/Html.svg",
-  },
-  {
-    id: 4,
-    name: "Javascript",
-    imgURL: "/skills/Javascript.svg",
-  },
-  {
-    id: 5,
-    name: "React",
-    imgURL: "/skills/React.svg",
-  },
-  {
-    id: 6,
-    name: "Tailwind",
-    imgURL: "/skills/Tailwind.svg",
-  },
-  {
-    id: 7,
-    name: "Typescript",
-    imgURL: "/skills/Typescript.svg",
-  },
+  { id: 1, name: "Java", imgURL: "/skills/Java.svg" },
+  { id: 2, name: "Javascript", imgURL: "/skills/Javascript.svg" },
+  { id: 3, name: "React", imgURL: "/skills/React.svg" },
+  { id: 4, name: "Angular", imgURL: "/skills/Angular.svg" },
+  { id: 5, name: "RxJs", imgURL: "/skills/RxJs.svg" },
+  { id: 6, name: "Redux", imgURL: "/skills/Redux.svg" },
+  { id: 7, name: "Git", imgURL: "/skills/Git.svg" },
+  { id: 8, name: "Github", imgURL: "/skills/Github.svg" },
+  { id: 9, name: "HTML", imgURL: "/skills/HTML.svg" },
+  { id: 10, name: "CSS", imgURL: "/skills/CSS.svg" },
+  { id: 11, name: "Tailwind", imgURL: "/skills/Tailwind.svg" },
+  { id: 12, name: "Firebase", imgURL: "/skills/Firebase.svg" },
+  { id: 13, name: "Supabase", imgURL: "/skills/Supabase.svg" },
+  { id: 14, name: "Postgresql", imgURL: "/skills/Postgresql.svg" },
+  { id: 15, name: "MySql", imgURL: "/skills/MySql.svg" },
 ];
 
 export function SkillsSection() {
